878ae4b209c4ccde050fb81e77e35fb9d71f48e2
santokoro <165536063+santokoro@users.noreply.github.com>
2026-03-25 23:25:48 +0700

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -4,7 +4,7 @@ echo "$(date): запуск мониторинга" >> ~/monitoring/log.txt
 AUTH_LOG="/var/log/auth.log"
 STATE_FILE="$HOME/monitoring/last_position.txt"
 
-# Если файла состояния нет — создаём
+
 if [ ! -f "$STATE_FILE" ]; then
     echo 0 > "$STATE_FILE"
 fi
@@ -12,25 +12,25 @@ fi
 LAST_POS=$(cat "$STATE_FILE")
 CURRENT_POS=$(stat -c%s "$AUTH_LOG")
 
-# Если файл не изменился — выходим
+
 if [ "$CURRENT_POS" -eq "$LAST_POS" ]; then
     exit 0
 fi
 
-# Читаем только новые строки
+
 NEW_LINES=$(tail -c +$((LAST_POS+1)) "$AUTH_LOG")
 
-# Фильтруем только настоящие входы
+
 NEW_LOGINS=$(echo "$NEW_LINES" | grep "login:session" | grep "session opened for user")
 
-# Если входов нет — выходим
+
 if [ -z "$NEW_LOGINS" ]; then
     echo "$CURRENT_POS" > "$STATE_FILE"
     exit 0
 fi
 
-# Отправляем сообщение
+
 /home/santoro/monitoring/send.py "$NEW_LOGINS"
 
-# Сохраняем новую позицию
+
 echo "$CURRENT_POS" > "$STATE_FILE"
